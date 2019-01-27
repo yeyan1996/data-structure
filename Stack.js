0eb2897832ab0434bb5e4739eb55ba59d71f49a3
yeyan1996 <1366792281@qq.com>
2019-01-27 23:28:54 +0800

    init commit

---
+++ Stack.js
@@ -0,0 +1,44 @@
+class Stack {
+    constructor() {
+        this.top = 0
+        this.dataStore = []
+    }
+
+    //推入一个元素
+    push(element) {
+        this.dataStore[this.top++] = element
+    }
+
+    //弹出并返回栈顶元素
+    pop() {
+        return this.dataStore[--this.top]
+    }
+
+    //返回栈顶元素
+    peek() {
+        return this.dataStore[this.top - 1]
+    }
+
+    clear() {
+        this.dataStore = []
+        this.top = 0
+    }
+
+    length() {
+        return this.top
+    }
+}
+
+let stack = new Stack()
+
+stack.push('a')
+stack.push('b')
+stack.push('c')
+
+console.log(stack)
+console.log(stack.peek())
+stack.clear()
+
+console.log(stack)
+
+console.log(stack.peek())
