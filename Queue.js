47c4bbfe99d1fb6fc0e8829c6d71eb6c8e62ea12
yeyan1996 <1366792281@qq.com>
2019-01-28 23:06:34 +0800

    feat:Queue

--- Queue.js
+++ Queue.js
@@ -1,3 +1,29 @@
 class Queue{
-
+    constructor() {
+        this.dataStore = []
+    }
+    //队尾插入一个元素
+    enqueue(item) {
+        this.dataStore.push(item)
+    }
+    //队首删除一个元素
+    dequeue() {
+        return this.dataStore.shift()
+    }
+    //队首的元素
+    front() {
+        return this.dataStore[0]
+    }
+    //队尾的元素
+    back() {
+        return this.dataStore[this.dataStore.length-1]
+    }
+    //返回队列
+    toString() {
+        return this.dataStore
+    }
+    //清空
+    empty() {
+        return this.dataStore.length === 0
+    }
 }
