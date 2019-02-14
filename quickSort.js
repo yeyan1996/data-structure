47d6ee85219b316c5f0507cfd94a9bae9a59a170
yeyan1996 <1366792281@qq.com>
2019-02-14 17:58:44 +0800

    update:快排使用标志点的位置由随机数代替固定的数组中间值

--- quickSort.js
+++ quickSort.js
@@ -1,10 +1,11 @@
-const MyArray = require('./MyArray.js')
-let arr = new MyArray()
-arr = [...arr] //hack
-
+// const MyArray = require('./MyArray.js')
+// let arr = new MyArray()
+// arr = [...arr] //hack
+arr = [59, 25, 71, 16, 62, 84, 34, 45]
 
 //创建新数组的快速排序(会占用额外控件)
 function quickSort1(arr) {
+    arr = [...arr]
     if (arr.length < 1) return arr
     let flagIndex = Math.floor(arr.length / 2)
     //需要先把标志元素从数组中拿出来,否则出现多个相同的标记元素会无法排序
@@ -24,13 +25,13 @@ console.log(quickSort1(arr))
 
 
 //原地算法的快速排序
- function quickSort2(arr, left, right) {
+function quickSort2(arr, left, right) {
     if (arr.length <= 1) return arr
     //返回一个新的选择点
     let flagIndex = partition(arr, left, right)
-    //排序左半边数组
+    //如果左边的元素超过1个则排序左半边数组
     if (left < flagIndex - 1) quickSort2(arr, left, flagIndex - 1)
-    //排序右半边数组(包括选择点元素)
+    //如果右边的元素超过1个则排序右半边数组(包括选择点元素)
     if (right > flagIndex) quickSort2(arr, flagIndex, right)
     return arr
 }
@@ -38,8 +39,11 @@ console.log(quickSort1(arr))
 function partition(arr, left, right) {
 
     //先指定一个选择点保存它的元素值,不能使用动态的flagIndex,否则每次判断都会改变
-    const pivot = arr[ Math.floor((left + right) / 2) ];
-    // let flagIndex = Math.floor((left + right) / 2)
+
+    //根据left/right参数设置一个随机下标点
+    let randomIndex = parseInt(Math.random() * (right - left + 1)) + left
+    const pivot = arr[randomIndex];
+
     while (left <= right) {
         //左哨兵和选择点进行对比,直到找到一个比选择点大的元素为止(找到选择点还没找到会停在选择点)
         while (arr[left] < pivot) {
@@ -49,7 +53,7 @@ function partition(arr, left, right) {
         while (arr[right] > pivot) {
             right--
         }
-        //Todo 研究一下这个为啥加个判断
+        //防止目标元素是整个元素最小的导致right的位置变成-1(这种情况则不会交换元素)
         if (left <= right) {
             [arr[left], arr[right]] = [arr[right], arr[left]]
             left++
