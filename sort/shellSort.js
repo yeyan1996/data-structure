8dfe77e9dd4b3c5c50b9b658fbf6a9b87c99a527
周浩磊 <1996yeyan@gmail.com>
2019-07-27 15:49:22 +0800

    update:修改希尔排序算法，使用双重循环 doc:添加希尔排序注释

--- sort/shellSort.js
+++ sort/shellSort.js
@@ -11,19 +11,20 @@ const MyArray = require('./MyArray.js')
 let arr = new MyArray()
 
 function shellSort() {
-    //计算间隔的算法
+    // 计算间隔的算法
     let h = 1
     while (h < arr.length / 3) {
         h = h * 3 + 1
     }
 
-    while (h > 0) {
-        //每次间隔为h,最后一次为正常的插入排序
-        for (let i = h; i < arr.length; i = i + h) {
-            let j = i
-            while (j > 0 && arr[j] < arr[j - h]) {
-                arr.swap(arr, j, j - h)
-                j = j - h
+    while (h > 0) {         // 每次间隔为 h ,最后一次为正常的插入排序
+        for (let j = 0; j < h; j++) { // 外层循环控制下标偏移量
+            for (let i = h + j; i < arr.length; i = i + h) { // 内层循环控制每次跳过 h 个元素
+                let j = i
+                while (j > 0 && arr[j] < arr[j - h]) {
+                    arr.swap(arr, j, j - h)
+                    j = j - h
+                }
             }
         }
         h = (h - 1) / 3
