e7163198952e9811157d9cd17c017eb975662020
周浩磊 <1996yeyan@gmail.com>
2019-09-09 10:09:26 +0800

    feat:添加递归版本的二分查找，查找第一个元素下标

--- sort/binSearch.js
+++ sort/binSearch.js
@@ -7,17 +7,16 @@
 
 let arr = [1, 2, 3, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9]
 
-function binSearch(arr, data) {
-    let startIndex = 0
-    let endIndex = arr.length - 1
-    while (startIndex <= endIndex) {
-        let midIndex = Math.floor((startIndex + endIndex) / 2)
+// 循环版本
+function binSearch_loop(arr, data, start, end) {
+    while (start <= end) {
+        let midIndex = Math.floor((start + end) / 2)
         let mid = arr[midIndex]
         if (data > mid) {
             //去除了中间的情况所以+1
-            startIndex = midIndex + 1
+            start = midIndex + 1
         } else if (data < mid) {
-            endIndex = midIndex - 1
+            end = midIndex - 1
         } else {
             return midIndex
         }
@@ -26,12 +25,29 @@ function binSearch(arr, data) {
     return -1
 }
 
-console.log(binSearch(arr, 9))
+// 递归版本
+function binSearch_recursive(arr, data, start, end) {
+    if (start > end) {
+        return -1
+    }
+    let mid = Math.floor((start + end) / 2)
+    if (arr[mid] < data) {
+        return binSearch_recursive(arr, data, mid + 1, end)
+    } else if (arr[mid] > data) {
+        return binSearch_recursive(arr, data, start, mid - 1)
+    } else if (arr[mid] === data) {
+        return mid
+    }
+}
+
+console.log(binSearch_loop(arr, 9, 0, arr.length - 1))
+console.log(binSearch_recursive(arr, 9, 0, arr.length - 1))
+
 
 //查找数组中某个元素出现次数
 function count(arr, data) {
     let count = 0
-    let pos = binSearch(arr, data)
+    let pos = binSearch_loop(arr, data, 0, arr.length - 1)
     if (pos > 0) {
         count++
     }
@@ -54,3 +70,32 @@ function count(arr, data) {
 }
 
 console.log(count(arr, 4))
+
+
+// 查找数组中第一个目标元素下标
+function firstIndex(arr, data) {
+    let start = 0
+    let end = arr.length - 1
+    while (start <= end) {
+        let midIndex = Math.floor((start + end) / 2)
+        let mid = arr[midIndex]
+        if (data > mid) {
+            start = midIndex + 1
+        } else if (data < mid) {
+            end = midIndex - 1
+        } else {
+            // 当前一个元素不是目标元素时
+            // 说明当前元素即使目标元素也是第一个目标元素
+            if(midIndex === arr.length -1 || arr[midIndex - 1] !== data){
+                return midIndex
+            }else{
+                // 否则继续循环
+                end = midIndex -1
+            }
+        }
+    }
+    //找不到返回-1
+    return -1
+}
+
+console.log(firstIndex(arr, 4))
