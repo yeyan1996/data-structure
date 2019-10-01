9ff2a18e878e65853ddf970979a22e12cae72bf4
周浩磊 <1996yeyan@gmail.com>
2019-10-01 16:34:03 +0800

    feat:二分查找最后一个目标元素 style:调整文件夹名字

--- algorithm/binSearch.js
+++ algorithm/binSearch.js
@@ -70,7 +70,7 @@ function count(arr, data) {
 
 console.log(count(arr, 4));
 
-// 查找数组中第一个目标元素下标
+// 查找数组中第一个目标元素的下标
 function firstIndex(arr, data) {
   let start = 0;
   let end = arr.length - 1;
@@ -82,12 +82,14 @@ function firstIndex(arr, data) {
     } else if (data < mid) {
       end = midIndex - 1;
     } else {
-      // 当前一个元素不是目标元素时
-      // 说明当前元素即使目标元素也是第一个目标元素
-      if (midIndex === arr.length - 1 || arr[midIndex - 1] !== data) {
+      // 当前面一个元素不是目标元素时
+      // 说明当前元素即是目标元素也是第一个目标元素
+      if (midIndex === 0 || arr[midIndex - 1] !== data) {
         return midIndex;
       } else {
         // 否则继续循环
+        // 由于是第一个目标元素
+        // 所以选择 midIndex 前半部分
         end = midIndex - 1;
       }
     }
@@ -97,3 +99,33 @@ function firstIndex(arr, data) {
 }
 
 console.log(firstIndex(arr, 4));
+
+// 查找数组中最后一个目标元素的下标
+function lastIndex(arr, data) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    const midIndex = Math.floor((start + end) / 2);
+    const mid = arr[midIndex];
+    if (data > mid) {
+      start = midIndex + 1;
+    } else if (data < mid) {
+      end = midIndex - 1;
+    } else {
+      // 当后面一个元素不是目标元素时
+      // 说明当前元素即是目标元素也是最后一个目标元素
+      if (midIndex === arr.length - 1 || arr[midIndex + 1] !== data) {
+        return midIndex;
+      } else {
+        // 否则继续循环
+        // 由于是第一个目标元素
+        // 所以选择 midIndex 前半部分
+        start = midIndex + 1;
+      }
+    }
+  }
+  // 找不到返回-1
+  return -1;
+}
+
+console.log(lastIndex(arr, 4));
